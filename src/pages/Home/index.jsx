e082c82dacd7ebec02a4213ef1ee7c7bc907c100
Elanis <elanis@hotmail.com>
2025-05-12 13:54:35 +0200

    <input>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Counter from "../../components/Counter";
 
 export default function Home() {
+	const [inputText, setInputText] = useState('');
+
 	const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 	const words = text.split(' ');
 
@@ -18,6 +21,18 @@ export default function Home() {
 
 	return (
 		<>
+			<input
+				type="text"
+				onChange={(e) => setInputText(e.target.value)}
+				value={inputText}
+			/>
+			<input
+				type="button"
+				onClick={() => setInputText('')}
+				value='Clear'
+			/>
+			<br />
+			<br />
 			<Counter />
 			<ul>{words.map((word, index) => <li key={index}>{word}</li>)}</ul>
 		</>
